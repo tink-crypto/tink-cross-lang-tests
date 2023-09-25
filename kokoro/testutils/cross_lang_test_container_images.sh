ee77bf160711c778ffe816c6b265b4f34001a185
Juerg Wullschleger <juerg@google.com>
2023-09-25 04:55:23 -0700

    Add valut to Dockerfile of cross langauge tests.
    We will be needing this to run HC vault integration tests.
    
    PiperOrigin-RevId: 568182498
    Change-Id: Ic6885fb5d0656d19a9fd1368881d499b43baa9af

--- kokoro/testutils/cross_lang_test_container_images.sh
+++ kokoro/testutils/cross_lang_test_container_images.sh
@@ -23,7 +23,7 @@ _image_prefix() {
 
 # Linux container images for cross language tests.
 readonly TINK_CROSS_LANG_BASE_IMAGE_NAME="linux-tink-cross-lang-base"
-readonly TINK_CROSS_LANG_BASE_IMAGE_HASH="309beb3f5860c829b5068346b677f8dc95b4fe17ba5c8626743c5ad89eb0d8f4"
+readonly TINK_CROSS_LANG_BASE_IMAGE_HASH="dd3cc25ab9ecbd212189e24762a4142940982a32a643f250247887be93a5d663"
 readonly TINK_CROSS_LANG_BASE_IMAGE="$(_image_prefix)/${TINK_CROSS_LANG_BASE_IMAGE_NAME}@sha256:${TINK_CROSS_LANG_BASE_IMAGE_HASH}"
 
 unset -f _image_prefix
