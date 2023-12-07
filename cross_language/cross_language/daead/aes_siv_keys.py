7918454bad7ad0138b971bb187874e44f8bd95aa
Thomas Holenstein <tholenst@google.com>
2023-12-07 10:12:51 -0800

    Add more AesSivKeys. This finishes what I wanted to test for AesSiv.
    PiperOrigin-RevId: 588831073
    Change-Id: If1fc8b98184d6d635a0c3f33087d19ed37152c5f

--- cross_language/cross_language/daead/aes_siv_keys.py
+++ cross_language/cross_language/daead/aes_siv_keys.py
@@ -14,6 +14,7 @@
 
 """Test keys for AES SIV."""
 
+import binascii
 import os
 from typing import Iterator, Tuple
 
@@ -44,6 +45,51 @@ def _proto_keys() -> Iterator[Tuple[str, bool, aes_siv_pb2.AesSivKey]]:
   yield ('Version 1 key (invalid)', False, key)
 
 
+def _proto_stress_test_key() -> test_key.TestKey:
+  """Returns a key using details of how proto serializes."""
+
+  # Proto will ignore unknown fields and use the last entry.
+  # 0x1801 is the serialization of a int32 field with tag 3 and value 1.
+  # 0x2001 is the serialization of a int32 field with tag 4 and value 1.
+  int32_tag3_value1 = binascii.unhexlify('1801')
+  # This will be ignored: when non-repeated fields repeat, proto uses the last
+  # instance
+  ignored_key = aes_siv_pb2.AesSivKey(
+      key_value=os.urandom(17),
+  ).SerializeToString()
+  key = aes_siv_pb2.AesSivKey(
+      key_value=os.urandom(64),
+  ).SerializeToString()
+  int32_tag4_value1 = binascii.unhexlify('2001')
+  return test_key.TestKey(
+      test_name='Proto stress test',
+      type_url='type.googleapis.com/google.crypto.tink.AesSivKey',
+      serialized_value=int32_tag3_value1
+      + ignored_key
+      + key  # Overwrites the "ignored_key"
+      + int32_tag4_value1,
+      key_material_type=tink_pb2.KeyData.KeyMaterialType.SYMMETRIC,
+      valid=True,
+  )
+
+
+def _key_with_output_prefix_type(
+    output_prefix_type: tink_pb2.OutputPrefixType,
+) -> test_key.TestKey:
+  output_prefix_type_name = tink_pb2.OutputPrefixType.Name(output_prefix_type)
+  return test_key.TestKey(
+      test_name=f'{output_prefix_type_name} key',
+      type_url='type.googleapis.com/google.crypto.tink.AesSivKey',
+      serialized_value=aes_siv_pb2.AesSivKey(
+          version=0,
+          key_value=os.urandom(64),
+      ).SerializeToString(),
+      key_material_type=tink_pb2.KeyData.KeyMaterialType.SYMMETRIC,
+      output_prefix_type=output_prefix_type,
+      valid=True,
+  )
+
+
 def aes_siv_keys() -> Iterator[test_key.TestKey]:
   """Returns test keys for HMac."""
   for (name, valid, msg) in _proto_keys():
@@ -54,6 +100,13 @@ def aes_siv_keys() -> Iterator[test_key.TestKey]:
         key_material_type=tink_pb2.KeyData.KeyMaterialType.SYMMETRIC,
         valid=valid,
     )
+
+  yield _key_with_output_prefix_type(tink_pb2.OutputPrefixType.RAW)
+  yield _key_with_output_prefix_type(tink_pb2.OutputPrefixType.TINK)
+  yield _key_with_output_prefix_type(tink_pb2.OutputPrefixType.CRUNCHY)
+  yield _key_with_output_prefix_type(tink_pb2.OutputPrefixType.LEGACY)
+  yield _proto_stress_test_key()
+
   # Proto-Unparseable value
   yield test_key.TestKey(
       test_name='Invalid proto-unparseable value',
