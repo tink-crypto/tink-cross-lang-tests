2f8f7b9e934762e8a166bb947d5f9ebaa36aeace
Thomas Holenstein <tholenst@google.com>
2024-09-11 05:06:31 -0700

    Check the version of the public key part, Ecies, Java.
    PiperOrigin-RevId: 673345961
    Change-Id: I01086737b82b79e6794184eeeb0afa5f0c8ef3b3

--- cross_language/cross_language/hybrid/creation_consistency_test.py
+++ cross_language/cross_language/hybrid/creation_consistency_test.py
@@ -68,7 +68,7 @@ class CreationConsistencyTest(absltest.TestCase):
     """Tests: Creation of HybridDecrypt from private key."""
     for key in hybrid_private_keys():
       for lang in tink_config.all_tested_languages():
-        if (lang == 'go' or lang == 'java') and 'b/365925769' in key.tags():
+        if lang == 'go' and 'b/365925769' in key.tags():
           continue
         supported = is_supported(key, lang)
         with self.subTest(f'{lang}, {key} ({supported})'):
@@ -93,7 +93,7 @@ class CreationConsistencyTest(absltest.TestCase):
     """
     for key in hybrid_private_keys():
       for lang in tink_config.all_tested_languages():
-        if (lang == 'go' or lang == 'java') and 'b/365925769' in key.tags():
+        if lang == 'go' and 'b/365925769' in key.tags():
           continue
         supported = is_supported(key, lang)
         with self.subTest(f'{lang}, {key} ({supported})'):
