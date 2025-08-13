69026ecb4e7e976ddae541911cec4973fbdf78f9
Thomas Holenstein <tholenst@google.com>
2025-08-13 02:13:57 -0700

    Use the newest Docker image (with Java 11) for running the cross language tests.
    PiperOrigin-RevId: 794474720
    Change-Id: Ib3d153645dd9eb57f7011f75dfbbbdea7c146062

--- kokoro/testutils/cross_lang_test_container_images.sh
+++ kokoro/testutils/cross_lang_test_container_images.sh
@@ -23,7 +23,7 @@ _image_prefix() {
 
 # Linux container images for cross language tests.
 readonly TINK_CROSS_LANG_BASE_IMAGE_NAME="linux-tink-cross-lang-base"
-readonly TINK_CROSS_LANG_BASE_IMAGE_HASH="b3e985dba817042f265a155e63424ef81e1c7ae497163fb7b4f74699e734ddc9"
+readonly TINK_CROSS_LANG_BASE_IMAGE_HASH="c51df4e0cad2cc4c0390161ee4c6954517fda8fe07845fc60b8cacc2d4e9ed90"
 readonly TINK_CROSS_LANG_BASE_IMAGE="$(_image_prefix)/${TINK_CROSS_LANG_BASE_IMAGE_NAME}@sha256:${TINK_CROSS_LANG_BASE_IMAGE_HASH}"
 
 unset -f _image_prefix
